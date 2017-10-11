21b8234d59b34f8ed5f9e63c72858df22bb08e17
Tomek <t.brylinski.twoheads@gmail.com>
2017-10-12 00:11:36 +0200

    Everything is done

--- nava_psd_template/scripts/custom.js
+++ nava_psd_template/scripts/custom.js
@@ -12,3 +12,19 @@ if (mediaQuery.matches) {
   selector.placeholder = 'Enter your destination';
 
 }
+
+/*
+================================================================================
+ Custom script for mobile menu
+================================================================================
+*/
+
+$('.menu_close_button').click(function() {
+  $('.mobile_menu').removeClass('active');
+  $('body').removeClass('lock');
+});
+
+$('.menu_button').click(function() {
+  $('.mobile_menu').addClass('active');
+  $('body').addClass('lock');
+});
